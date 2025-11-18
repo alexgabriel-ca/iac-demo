c061b7137c9b3ad7560c34610650a85a3e77d12a
Alex Gabriel <alex@alexgabriel.ca>
2025-11-18 10:49:56 -0500

    First Commit

---
+++ igw.tf
@@ -0,0 +1,24 @@
+resource "aws_internet_gateway" "central_ig" {
+  provider = aws.central
+  vpc_id   = aws_vpc.central_vpc.id
+
+  depends_on = [
+    aws_subnet.central_public_subnet
+  ]
+  tags = {
+    Name = "OpenText Central Internet Gateway"
+    Environment = var.environment
+  }
+}
+
+resource "aws_internet_gateway" "west_ig" {
+  provider = aws.west
+  vpc_id   = aws_vpc.west_vpc.id
+  depends_on = [
+    aws_subnet.west_public_subnet
+  ]
+  tags = {
+    Name = "OpenText West Internet Gateway"
+    Environment = var.environment
+  }
+}
